627e75fbdec9ed26e584babe49bc3e190ac81985
Arjun Ghimire <ghimirearjun15@gmail.com>
2021-03-21 23:29:28 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts",
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+}
+);
 
 // routes
 require("./routes/htmlroutes.js")(app);
